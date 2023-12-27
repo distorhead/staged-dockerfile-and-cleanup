22d7a0c176da1e6657497096c930b0663e42085d
Your Name <you@example.com>
2023-12-27 14:02:27 +0000

    ++

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-werf cleanup --repo ghcr.io/distorhead/staged-dockerfile-and-cleanup-1 --without-kube &
+werf cleanup --repo ghcr.io/distorhead/staged-dockerfile-and-cleanup-1 --without-kube --repo-github-token $GITHUB_TOKEN &
 werf build --repo ghcr.io/distorhead/staged-dockerfile-and-cleanup-1 &
