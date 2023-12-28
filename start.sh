f209fba807b3820b34ed1ad7fe5752722a59d8f3
Your Name <you@example.com>
2023-12-28 11:16:26 +0000

    ++

--- start.sh
+++ start.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-echo uuidgen > id
-git add id
-git commit -m "$(cat id)"
-
-werf build --repo ghcr.io/distorhead/staged-dockerfile-and-cleanup-1 | tee build.log &
-sleep 1
 werf cleanup --repo ghcr.io/distorhead/staged-dockerfile-and-cleanup-1 --without-kube --repo-github-token $GITHUB_TOKEN | tee cleanup.log &
+werf build --repo ghcr.io/distorhead/staged-dockerfile-and-cleanup-1 | tee build.log &
